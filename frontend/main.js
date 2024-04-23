2c3a73d5f885816876df9cb0ac7c83ad9948b3b3
Srijan-D <srijandubey3105@gmail.com>
2024-04-23 16:02:43 +0530

    configuring siriwave and styling

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,33 @@ $(document).ready(function () {
         },
         out: {
             effect: "bounceOut",
+        }
+
+    });
+
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "2",
+        speed: "0.10",
+        autostart: true
+
+    });
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
         },
 
     });
+
 });
+
