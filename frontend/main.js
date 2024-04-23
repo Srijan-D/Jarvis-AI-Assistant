21a7f618ba989ecae581ff292c528f51288f8a7b
Srijan-D <srijandubey3105@gmail.com>
2024-04-23 11:55:02 +0530

    textillate jquery, setting up eel virtual environment

---
+++ frontend/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+});
