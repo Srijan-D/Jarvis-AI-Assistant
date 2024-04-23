728e3e3753b17357355444875dfb66020a2189db
Srijan-D <srijandubey3105@gmail.com>
2024-04-23 14:27:17 +0530

    configuring eel to serve static file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import os
 import eel
+
+    
+eel.init("frontend")
+
+
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')   
+# this opens the browser with the localhost server
+
+eel.start('index.html', mode=None, host='localhost', block=True)
+# this starts server at localhost and serves the index.html file
