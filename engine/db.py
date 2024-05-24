8c1b013f16c0d75a85364fb972e8e26db10faabb
Srijan-D <srijandubey3105@gmail.com>
2024-05-25 02:43:00 +0530

    testing sqlite query and helper function

--- engine/db.py
+++ engine/db.py
@@ -25,15 +25,25 @@ cursor=con.cursor() # cursor object is used to interact with the database
 
 # Specify the column indices you want to import (0-based index)
 # Example: Importing the 1st and 3rd columns
-desired_columns_indices = [0, 32]
+# desired_columns_indices = [0, 33]
 
-# Read data from CSV and insert into SQLite table for the desired columns
-with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        selected_data = [row[i] for i in desired_columns_indices]
-        cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
 
 # Commit changes and close connection
-con.commit()
-con.close()
+# con.commit()
+# con.close()
+
+
+# searching query inside the contacts
+
+query="srijan"
+query=query.strip().lower()
+
+cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?",('%' + query + '%', query + '%'))
+results=cursor.fetchall()
+print(results[0][0])
--- engine/helper.py
+++ engine/helper.py
@@ -7,3 +7,15 @@ def extract_yt_term(command):
     match=re.search(pattern,command,re.IGNORECASE)
     # if match found return song name
     return match.group(1) if match else None
+
+def remove_words(input_string, words_to_remove):
+    # Split the input string into words
+    words = input_string.split()
+
+    # Remove unwanted words
+    filtered_words = [word for word in words if word.lower() not in words_to_remove]
+
+    # Join the remaining words back into a string
+    result_string = ' '.join(filtered_words)
+
+    return result_string
